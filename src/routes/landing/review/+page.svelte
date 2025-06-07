97eee466e4f00b5e5c24ebdfb610b256fde0e95b
Toph Tucker <tophtucker@gmail.com>
2025-06-07 19:47:38 -0400

    resolves #94

---
+++ src/routes/landing/review/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+	import Header from '../../Header.svelte';
+	import Footer from '../../Footer.svelte';
+</script>
+
+<svelte:head>
+	<title>Thanks for staying! • Apple Tree Inn • Lenox, MA</title>
+</svelte:head>
+
+<Header />
+
+<div class="inner">
+	<h1>We are so glad you enjoyed your stay!</h1>
+
+	<p>
+		If you have an extra minute, would you mind leaving us a Google review? Your feedback not only
+		helps us grow — it also helps future guests discover the Inn.
+	</p>
+
+	<p>
+		👉 <a
+			href="https://www.google.com/maps/place/Apple+Tree+Inn/@42.3533478,-73.3194824,17z/data=!3m1!4b1!4m9!3m8!1s0x89e75b0d1fb4a8f3:0x79a8a337c2d8eb8f!5m2!4m1!1i2!8m2!3d42.3533439!4d-73.3169021!16s%2Fg%2F1xn_pds9?entry=ttu&g_ep=EgoyMDI1MDUyNi4wIKXMDSoASAFQAw%3D%3D"
+			>Leave a review for the Inn</a
+		>
+	</p>
+
+	<p>
+		👉 <a
+			href="https://www.google.com/maps/place/The+Ostrich+Room/@42.3534222,-73.3193781,17z/data=!4m16!1m9!3m8!1s0x89e75b2380845a13:0x7d89b7128cec2fc2!2sThe+Ostrich+Room!8m2!3d42.3534183!4d-73.3167978!9m1!1b1!16s%2Fg%2F11pv_jxd86!3m5!1s0x89e75b2380845a13:0x7d89b7128cec2fc2!8m2!3d42.3534183!4d-73.3167978!16s%2Fg%2F11pv_jxd86?entry=ttu&g_ep=EgoyMDI1MDUyNi4wIKXMDSoASAFQAw%3D%3D"
+			>Leave a review for the Ostrich Room</a
+		>
+	</p>
+
+	<p>It was great having you. We hope to welcome you back soon!</p>
+
+	<p>Warmly,<br />The staff at the Apple Tree Inn</p>
+</div>
+
+<Footer />
