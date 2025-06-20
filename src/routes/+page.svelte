abfb944a4c4124e8eea10c8121f79ffc9fcb6c6f
Toph Tucker <tophtucker@gmail.com>
2025-06-20 17:43:53 -0400

    homepage source cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script>
 	import Frame from '$lib/template/Frame.svelte';
-	import Asterisk from '$lib/icons/Asterisk.svelte';
-	import Image from '$lib/lightbox/Image.svelte';
 	import GalleryGrid from '$lib/components/GalleryGrid.svelte';
 </script>
 
@@ -19,55 +17,13 @@
 			<img src="./avaloch/img/door.jpg" alt="Door" />
 			<img src="./avaloch/img/fireplace.jpg" alt="Fireplace" />
 		</div>
-
 		<div class="section"><GalleryGrid /></div>
-
-		<!-- <div class="gallery">
-			<Image src="/avaloch/img/firepit.jpg" alt="Firepit" class="square" />
-			<Image src="/avaloch/img/lawn-snow.jpg" alt="The snowy lawn" class="square" />
-			<Image src="/avaloch/img/music.jpg" alt="The Ostrich Room" class="square" />
-			<Image src="/img/cocktail.jpg" alt="One of Sean’s cocktails" class="square" />
-			<Image src="/heropics/pool1.jpg" alt="The pool" class="square" />
-			<a href="/about/gallery"><span>Gallery</span></a>
-		</div> -->
 	</div>
 </Frame>
 
 <style>
 	.section {
 		margin: 1rem 0;
-	}
-	.gallery {
-		margin: 2rem 0;
-		display: grid;
-		grid-template-columns: repeat(6, 1fr);
-		gap: 1rem;
-		font-family: watkins;
-	}
-	.gallery a {
-		/* border: 1px solid black; */
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		font-size: 14px;
-		text-decoration: none;
-		color: inherit;
-		transition: color 0.3s;
-
-		background-image: url('/avaloch/img/gallery.png');
-		background-size: cover;
-	}
-	.gallery a span {
-		padding: 10px 6px 8px 6px;
-		background: white;
-	}
-	.gallery a:hover {
-		color: var(--blue);
-	}
-	.blurbs {
-		font-family: watkins;
-		text-align: center;
-		margin: 2rem 0;
 	}
 	.img-pair {
 		display: grid;
