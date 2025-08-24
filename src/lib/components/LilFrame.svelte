390e99c9c6f4538d578fe83db15aafc7560343d1
Toph Tucker <tophtucker@gmail.com>
2025-08-24 03:22:42 -0400

    testing out more structured homepage

---
+++ src/lib/components/LilFrame.svelte
@@ -0,0 +1,51 @@
+<script>
+	let { children, accent } = $props();
+</script>
+
+<div class="wrapper" style={`--accent: ${accent}`}>
+	<div class="filled"></div>
+	<div class="left-right"></div>
+	<div class="filled"></div>
+
+	<div class="top-bottom"></div>
+	<div class="content">{@render children()}</div>
+	<div class="top-bottom"></div>
+
+	<div class="filled"></div>
+	<div class="left-right"></div>
+	<div class="filled"></div>
+</div>
+
+<style>
+	.wrapper {
+		border: 1px solid var(--accent);
+		display: inline-grid;
+		grid-template-columns: 4px 1fr 4px;
+		grid-template-rows: 4px 1fr 4px;
+	}
+
+	.wrapper .content {
+		border: 1px solid var(--accent);
+		padding: 1rem;
+		text-align: center;
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		background: var(--accent);
+		color: white;
+	}
+
+	.wrapper .filled {
+		background: var(--accent);
+	}
+
+	.wrapper .top-bottom {
+		border-top: 1px solid var(--accent);
+		border-bottom: 1px solid var(--accent);
+	}
+
+	.wrapper .left-right {
+		border-left: 1px solid var(--accent);
+		border-right: 1px solid var(--accent);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 	import GalleryGrid from '$lib/components/GalleryGrid.svelte';
+	import HR from '$lib/components/HR.svelte';
+	import LilFrame from '$lib/components/LilFrame.svelte';
 	import heroDrone from '$lib/assets/hero-drone.jpg?enhanced';
 
 	console.log('toph', { heroDrone });
@@ -15,12 +17,59 @@
 
 <div class="inner main">
 	<enhanced:img src={heroDrone} alt="The Apple Tree Inn’s Main House" />
+
+	<div class="tagline">
+		<HR />
+		<h2>For the young who are old are heart<br />and the old who are young at heart</h2>
+		<HR />
+	</div>
+
+	<div class="tout">
+		<img src="/roompics/main-house/8/_XCS2672.jpg" />
+		<LilFrame accent="var(--green)">
+			<div class="tout-inner">
+				<h2>The living room<br />of the Berkshires</h2>
+				<p>
+					For seventy years, this 1885 house has been welcoming guests to stay and dine directly
+					across the street from Tanglewood.
+				</p>
+				<p><a href="/rooms" class="cta">See rooms</a></p>
+			</div>
+		</LilFrame>
+	</div>
+
+	<div class="tout">
+		<LilFrame accent="var(--red)">
+			<div class="tout-inner">
+				<h2>Not fancy</h2>
+				<p>
+					The Ostrich Room’s drinks run the gamut from $4 Bud to Sean’s much more elevated
+					cocktails.
+				</p>
+				<p><a href="/dining/ostrich-room" class="cta">See the menu</a></p>
+			</div>
+		</LilFrame>
+		<img src="/avaloch/img/ostrich-room-packed.jpg" />
+	</div>
+
+	<div class="tout">
+		<img src="/avaloch/img/wanda.jpg" />
+		<LilFrame accent="var(--blue)">
+			<div class="tout-inner">
+				<h2>Second-best show<br />on the block</h2>
+				<p>
+					We may not draw 18,000 people, but locals love the deep bench of musical talent in the
+					Berkshires.
+				</p>
+				<p><a href="/dining/ostrich-room" class="cta">See the schedule</a></p>
+			</div>
+		</LilFrame>
+	</div>
+
 	<div class="img-pair">
 		<img src="./avaloch/img/door.jpg" alt="Door" loading="lazy" />
 		<img src="./avaloch/img/fireplace.jpg" alt="Fireplace" loading="lazy" />
 	</div>
-	<GalleryGrid />
-	<img src="/avaloch/img/porch-breakfast.jpg" alt="Breakfast on the porch" loading="lazy" />
 </div>
 
 <style>
@@ -29,6 +78,42 @@
 		flex-direction: column;
 		gap: 1rem;
 	}
+
+	.tagline {
+		text-align: center;
+	}
+
+	.tout {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		gap: 1rem;
+		text-wrap: pretty;
+		text-align: center;
+	}
+
+	.tout h2,
+	.tout p {
+		margin: 0;
+	}
+
+	.tout p {
+		font-size: larger;
+	}
+
+	.tout-inner {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: 1rem;
+		height: 100%;
+	}
+
+	.tout img {
+		object-fit: cover;
+		aspect-ratio: 4 / 3;
+	}
+
 	.img-pair {
 		display: grid;
 		grid-template-columns: 1fr 1fr;
