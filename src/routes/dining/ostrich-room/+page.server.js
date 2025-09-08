b85624741b808bcb3190994d8cfbd3acf28dd14e
Toph Tucker <tophtucker@gmail.com>
2025-09-08 00:22:39 -0400

    ok im not sure but i think previously if the server didnt restart, the query wouldnt get its updated parameters. so fixed that

--- src/routes/dining/ostrich-room/+page.server.js
+++ src/routes/dining/ostrich-room/+page.server.js
@@ -2,11 +2,10 @@ import { Temporal } from '@js-temporal/polyfill';
 import { sanity, parsePerformance } from '$lib/sanity.js';
 import { TIME_ZONE } from '$lib/index.js';
 
-const today = Temporal.Now.plainDateISO(TIME_ZONE);
-const latestMonday = today.subtract({ days: (today.dayOfWeek + 6) % 7 });
-const nextMonday = latestMonday.add({ days: 7 });
-
-const QUERY = `*[_type == "performance" && startTime > '${latestMonday.toString()}' && startTime < '${nextMonday.toString()}'] | order(startTime asc) {
+const QUERY = (
+	start,
+	end
+) => `*[_type == "performance" && startTime > '${start}' && startTime < '${end}'] | order(startTime asc) {
   _id,
   startTime,
   endTime,
@@ -23,5 +22,12 @@ const QUERY = `*[_type == "performance" && startTime > '${latestMonday.toString(
 }`;
 
 export async function load() {
-	return { performances: (await sanity.fetch(QUERY)).map(parsePerformance) };
+	const today = Temporal.Now.plainDateISO(TIME_ZONE);
+	const latestMonday = today.subtract({ days: (today.dayOfWeek + 6) % 7 });
+	const nextMonday = latestMonday.add({ days: 7 });
+	return {
+		performances: (await sanity.fetch(QUERY(latestMonday.toString(), nextMonday.toString()))).map(
+			parsePerformance
+		)
+	};
 }
