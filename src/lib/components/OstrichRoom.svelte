03857862fda3f7f17264f63466194fc3cdeb61e2
Toph Tucker <tophtucker@gmail.com>
2025-10-30 18:29:28 -0400

    use LilFrame to tout Ostrich Room on music page; this component could be better, but this kind of thing is very important!!

--- src/lib/components/OstrichRoom.svelte
+++ src/lib/components/OstrichRoom.svelte
@@ -1,25 +1,13 @@
 <script>
+	import LilFrame from '$lib/components/LilFrame.svelte';
 	let { ostrichRoom } = $props();
 </script>
 
-<!-- TODO refactor to use LilFrame component -->
-<div class="wrapper">
-	<div class="filled"></div>
-	<div class="left-right"></div>
-	<div class="filled"></div>
-
-	<div class="top-bottom"></div>
-	<div class="content">
-		<h3>The<br />Ostrich<br />Room</h3>
-		<div>{ostrichRoom.dayRange}</div>
-		<a class="cta" href="/dining/ostrich-room">More info</a>
-	</div>
-	<div class="top-bottom"></div>
-
-	<div class="filled"></div>
-	<div class="left-right"></div>
-	<div class="filled"></div>
-</div>
+<LilFrame accent="var(--red)">
+	<h3>The<br />Ostrich<br />Room</h3>
+	<div>{ostrichRoom.dayRange}</div>
+	<a class="cta" href="/dining/ostrich-room">More info</a>
+</LilFrame>
 
 <style>
 	h3 {
@@ -28,35 +16,5 @@
 
 	a.cta {
 		align-self: center;
-	}
-
-	.wrapper {
-		border: 1px solid black;
-		display: inline-grid;
-		grid-template-columns: 4px 1fr 4px;
-		grid-template-rows: 4px 1fr 4px;
-	}
-
-	.wrapper .content {
-		border: 1px solid black;
-		padding: 1rem;
-		text-align: center;
-		display: flex;
-		flex-direction: column;
-		gap: 1rem;
-	}
-
-	.wrapper .filled {
-		background: black;
-	}
-
-	.wrapper .top-bottom {
-		border-top: 1px solid black;
-		border-bottom: 1px solid black;
-	}
-
-	.wrapper .left-right {
-		border-left: 1px solid black;
-		border-right: 1px solid black;
 	}
 </style>
