4df7a3581ba236edc454e6ea28662a1b76ea8deb
Toph Tucker <tophtucker@gmail.com>
2026-03-18 01:19:39 -0400

    moving routes around to have a lodge page outside the main layout. checkpoint, barebones lodge page.

---
+++ src/routes/lodge/+page@.svelte
@@ -0,0 +1,49 @@
+<script>
+	import Avaloch from '$lib/icons/Avaloch.svelte';
+	import HR from '$lib/components/HR.svelte';
+</script>
+
+<main>
+	<header>
+		<Avaloch class="avaloch" />
+		<h1>Lodge</h1>
+	</header>
+
+	<p>is the only lodging within walking distance to Tanglewood.</p>
+	<p>
+		Perched on a big hill overlooking the Stockbridge Bowl, it has a view, a pool, a coffee machine,
+		and almost nothing else.
+	</p>
+
+	<a class="cta" href="https://direct-book.com/properties/AppleTreeInndirect">Book now</a>
+</main>
+
+<style>
+	main {
+		padding: 2em;
+		font-size: 4em;
+		background: var(--green);
+		color: white;
+	}
+
+	h1 {
+		margin: 0;
+		text-box-trim: trim-both;
+		margin-top: -0.4em;
+		font-size: 5vw;
+	}
+
+	p {
+		max-width: 15em;
+	}
+
+	.cta {
+		height: auto;
+		padding: 0.2em 0.4em;
+		border-width: 8px;
+	}
+
+	:global(.avaloch) {
+		height: 15vw;
+	}
+</style>
