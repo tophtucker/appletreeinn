3176a03dc9811bf3f539c02bd31dfe436189eb26
Toph Tucker <tophtucker@gmail.com>
2026-03-19 01:33:53 -0400

    contact info, coming soon notice

--- src/routes/lodge/+page.svelte
+++ src/routes/lodge/+page.svelte
@@ -1,7 +1,15 @@
 <script>
 	import Avaloch from '$lib/icons/Avaloch.svelte';
-	import HR from '$lib/components/HR.svelte';
+	import Pentathing from '$lib/icons/Pentathing.svelte';
+	import Doorknob from '$lib/icons/Doorknob.svelte';
+	import Rule from './Rule.svelte';
 </script>
+
+<svelte:head>
+	<title>Avaloch Lodge • Lenox, Mass.</title>
+</svelte:head>
+
+<Rule />
 
 <header>
 	<div class="hero-img-wrap">
@@ -27,27 +35,63 @@
 	</div>
 </header>
 
-<main>
-	<p>is the only lodging within walking distance to Tanglewood.</p>
+<Rule />
+
+<section class="first">
+	<p>is the only lodging within walking distance to Tanglewood<sup>(9 min.)</sup>.</p>
 	<p>
 		Perched on a big hill overlooking the Stockbridge Bowl, it has a view, a pool, a coffee machine,
 		and almost nothing else.
 	</p>
 
 	<a class="cta" href="https://direct-book.com/properties/AppleTreeInndirect">Book now</a>
-</main>
+</section>
+
+<Rule />
+
+<section>
+	<div style="display: flex; justify-content: space-between; align-items: center;">
+		<div>
+			<div>10 Richmond Mountain Road</div>
+			<div>Lenox, Massachusetts 01240</div>
+			<div>
+				<a href="mailto:frontdesk@avalochinn.com" target="_blank">frontdesk@avalochinn.com</a>
+			</div>
+			<div>(413) 637-1910</div>
+			<div><a href="https://www.instagram.com/avaloch.inn/" target="_blank">@avaloch.inn</a></div>
+		</div>
+		<Pentathing height="100" width="100" altFill="var(--green)" />
+	</div>
+</section>
+
+<Rule />
+
+<section>
+	<div class="coming-soon">Coming Fall 2026</div>
+	<p>
+		The <span class="inalign">Avaloch Inn</span> and <span class="inalign">Café</span> and
+		<span class="inalign">Ostrich Room</span> are currently closed for renovations. The whole main
+		house of the former
+		<em>Apple Tree Inn</em> will miss the summer season. Claire and Christian and co. are going as fast
+		as they can and expect to reopen fully this fall. There will be construction onsite this summer.
+	</p>
+</section>
 
 <style>
 	:global(body) {
-		font-size: 4em;
+		font-size: 2em;
 		background: var(--green);
-		color: white;
+		color: rgba(255, 255, 255, 0.85);
 		font-family: 'Align Web';
 	}
 
-	main {
+	section {
 		max-width: 980px;
-		margin: 0 auto;
+		margin: 4rem auto;
+	}
+
+	section.first {
+		font-size: 2em;
 	}
 
 	header {
@@ -56,12 +100,45 @@
 
 	h1 {
 		margin: 0;
-		margin-top: -0.6em;
+		margin-top: -0.4em;
 		font-size: 5vw;
 	}
 
 	p {
-		max-width: 15em;
+		max-width: 100%;
+	}
+
+	a {
+		color: inherit;
+	}
+
+	sup {
+		font-size: 20px;
+		position: absolute;
+		padding-left: 5px;
+		padding-top: 10px;
+	}
+
+	a:hover {
+		color: white;
+	}
+
+	.inalign {
+		font-family: 'Align Web';
+		font-variation-settings: 'wght' 800;
+		text-transform: uppercase;
+		color: white;
+	}
+
+	.coming-soon {
+		border: 3px double currentColor;
+		padding: 0.25rem 0.5rem;
+		font-size: smaller;
+		color: var(--green);
+		background: white;
+		text-transform: uppercase;
+		display: inline-block;
+		letter-spacing: 2px;
 	}
 
 	.cta {
@@ -72,13 +149,14 @@
 		background: inherit;
 		color: inherit;
 	}
-	main .cta:hover {
+
+	section .cta:hover {
 		background: rgba(255, 255, 255, 0.8);
 		color: var(--green);
 	}
 
 	:global(.avaloch) {
-		height: 15vw;
+		height: 180px;
 	}
 
 	.wordmark {
