16b23dc8101cce7bd919a922611b1b7df6521ff8
Toph Tucker <tophtucker@gmail.com>
2026-04-13 20:52:28 -0400

    restore amenities to nav

--- src/lib/nav.js
+++ src/lib/nav.js
@@ -4,6 +4,7 @@ export const RESERVATIONS_URL = '#'; // 'https://tables.toasttab.com/restaurants
 export const nav = [
 	{ title: 'Renovations', slug: 'about/renovations' },
 	{ title: 'Gallery', slug: 'overview/gallery' },
+	{ title: 'Amenities', slug: 'activities/amenities' },
 	{ title: 'Tanglewood', slug: 'activities/tanglewood' },
 	{ title: 'The Berkshires', slug: 'activities/berkshires' },
 	{ title: 'Property map', slug: 'overview/map' },
